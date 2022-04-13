f51ea8c0265dca2a3ccddac8411c3a6702fa828f
Haomiao Shi <hxs649@case.edu>
2022-04-12 21:47:05 -0400

    run on pycharm

--- onlineStore/onlineStore/settings.py
+++ onlineStore/onlineStore/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+# register/intall subreference(子应用)
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'book' # plan 1
+    'commodity.apps.CommodityConfig',  # plan 2
 ]
 
 MIDDLEWARE = [
